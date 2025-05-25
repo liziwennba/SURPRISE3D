7489fa1e72ea455f05e7ae034468cef1d3bf53bd
liziwen <liziwennba@gmail.com>
2025-05-25 14:33:56 +0400

    Main change

--- Models/intent3d/start.sh
+++
@@ -1,5 +0,0 @@
-export HF_ENDPOINT="https://hf-mirror.com"
-export HF_HOME=/workspace/huggingface
-source /opt/conda/bin/activate /opt/conda/envs/Intent3D
-sh init.sh
-sh scripts/train_scanintend.sh
--- Models/reason3d/start.sh
+++
@@ -1,4 +0,0 @@
-export HF_ENDPOINT="https://hf-mirror.com"
-export HF_HOME=/workspace/huggingface
-source /opt/conda/bin/activate /opt/conda/envs/spatial
-python -m torch.distributed.run --nproc_per_node=8 --master_port=29501 train.py --cfg-path lavis/projects/reason3d/train/reason3d_scanrefer_scratch.yaml
